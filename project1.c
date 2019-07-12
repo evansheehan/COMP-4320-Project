4427348a3a0e76fb735741c37097790e44f075d7
res0038 <res0038@tigermail.auburn.edu>
2019-07-12 00:06:16 -0500

    Added a few non-functional items

---
+++ project1.c
@@ -0,0 +1,21 @@
+//Project 1
+//COMP 4320
+//Professor Alvin Lim
+//Group 2
+
+/*#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>*/
+
+int main(void)
+{
+
+    printf("Test!");
+    return 0;
+}
+
+//Internet address structure
+struct int_addr
+{
+    unsigned int s_addr;
+};
