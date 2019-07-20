a9eab45cd4af5c4f7d6d85c2dcbca6eac5eeeb04
awubis <awubis@gmail.com>
2019-07-20 18:34:24 -0500

    Working on file handling for FTP client-side

--- client.c
+++ client.c
@@ -11,12 +11,43 @@
 #include <netdb.h>
 #include <unistd.h>
 
+#define fileName "testFile.txt"
+
 int clientSock;
 struct sockaddr_in client;
 
 int main(void)
 {
-    client.sin_family = AF_INET;
+    //Read file and put in buffer
+    //Adapted from http://www.fundza.com/c4serious/fileIO_reading_all/
+    FILE* file = fopen(fileName, "r");
+
+    //Check if file is successfully created
+    if (file == NULL) {
+        printf("Couldn't find specified file...Closing program");
+        return 1;
+    }
+
+    //Gets number of bytes for file
+    fseek(file, 0L, SEEK_END);
+    long numBytes = ftell(file);
+    printf("%d", numBytes);
+
+    //Resets file position indicator to beginning of file
+    fseek(file, 0L, SEEK_SET);
+
+    //char* buffer = (char*)calloc(numBytes, sizeof(char));
+
+
+
+
+
+
+
+
+
+
+    /*client.sin_family = AF_INET;
     client.sin_addr.s_addr = htonl(INADDR_ANY);
     client.sin_port = htons(12345); //Should this be the FTP port?
 
@@ -25,8 +56,11 @@ int main(void)
     int clientSock = socket(AF_INET, SOCK_DGRAM, 0);
     bind(clientSock, (struct sockaddr*)&client, sizeof(client));
 
-    FILE* file = fopen("testFile.txt", "r");
+    close(clientSock);*/
+    return 0;
+}
 
-    close(clientSock);
+int clientProcess() {
+
     return 0;
 }
--- server.c
+++ server.c
@@ -2,3 +2,12 @@
 // Created by Evan Sheehan on 2019-07-16.
 //
 
+int main(void) {
+
+    return 0;
+}
+
+int serverProcess() {
+
+    return 0;
+}
